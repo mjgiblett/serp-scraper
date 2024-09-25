fca0d7f4f1946a7e6dc2c4e09b49d05a5b8cda22
Matthew Giblett <matthewgiblett@outlook.com>
2024-09-26 09:43:16 +1000

    Added main function

--- main.py
+++ main.py
@@ -1,44 +1,51 @@
 import os
+from datetime import datetime
 
 import requests
 from dotenv import load_dotenv, set_key
 from pandas import DataFrame, ExcelWriter
 
 from src.constants import (
+    DATA_PATH,
     DOTENV_PATH,
     KEY_API_PASSWORD,
     KEY_API_USERNAME,
     REQUEST_PAYLOAD,
 )
 
-load_dotenv()
+
+def main() -> None:
+    load_dotenv()
+
+    if KEY_API_USERNAME not in os.environ:
+        os.environ[KEY_API_USERNAME] = input("Username: ")
+        set_key(DOTENV_PATH, KEY_API_USERNAME, os.environ[KEY_API_USERNAME])
+    if KEY_API_PASSWORD not in os.environ:
+        os.environ[KEY_API_PASSWORD] = input("Password: ")
+        set_key(DOTENV_PATH, KEY_API_PASSWORD, os.environ[KEY_API_PASSWORD])
+
+    response = requests.request(
+        "POST",
+        "https://realtime.oxylabs.io/v1/queries",
+        auth=(os.environ[KEY_API_USERNAME], os.environ[KEY_API_PASSWORD]),
+        json=REQUEST_PAYLOAD,
+    )
+
+    pages = response.json()["results"]
+
+    organic_results = []
+    for page, data in enumerate(pages):
+        for x in data["content"]["results"]["organic"]:
+            i = (page * 10) + int(x["pos"])
+            organic_results.append(
+                {"pos": i, "url": x["url"], "title": x["title"], "desc": x["desc"]}
+            )
+
+    df = DataFrame(organic_results)
+    writer_path = f"{DATA_PATH}{datetime.now().strftime("%d-%m-%Y-%H-%M-%S")}.xlsx"
+    with ExcelWriter(writer_path) as writer:
+        df.to_excel(writer)
 
 
-if KEY_API_USERNAME not in os.environ:
-    os.environ[KEY_API_USERNAME] = input("Username: ")
-    set_key(DOTENV_PATH, KEY_API_USERNAME, os.environ[KEY_API_USERNAME])
-if KEY_API_PASSWORD not in os.environ:
-    os.environ[KEY_API_PASSWORD] = input("Password: ")
-    set_key(DOTENV_PATH, KEY_API_PASSWORD, os.environ[KEY_API_PASSWORD])
-
-
-response = requests.request(
-    "POST",
-    "https://realtime.oxylabs.io/v1/queries",
-    auth=(os.environ[KEY_API_USERNAME], os.environ[KEY_API_PASSWORD]),
-    json=REQUEST_PAYLOAD,
-)
-
-pages = response.json()["results"]
-
-organic_results = []
-for page, data in enumerate(pages):
-    for x in data["content"]["results"]["organic"]:
-        i = (page * 10) + int(x["pos"])
-        organic_results.append(
-            {"pos": i, "url": x["url"], "title": x["title"], "desc": x["desc"]}
-        )
-
-df = DataFrame(organic_results)
-with ExcelWriter("data/export.xlsx") as writer:
-    df.to_excel(writer)
+if __name__ == "__main__":
+    main()
